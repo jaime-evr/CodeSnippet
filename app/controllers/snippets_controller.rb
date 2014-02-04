0b4c568eaa0f752d2938e933fc9eb956416651a1
Jaime Victoria <jaime.victoria@crowdint.com>
2014-02-04 13:56:50 -0600

    added new sidekiq process to manage request

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -7,9 +7,7 @@ class SnippetsController < ApplicationController
   def create
     @snippet = Snippet.new(permited_params)
     if @snippet.save
-      uri = URI.parse("http://pygments.appspot.com/")
-      request = Net::HTTP.post_form(uri, lang: @snippet.language, code: @snippet.plain_code)
-      @snippet.update_attribute(:highlighted_code, request.body)
+      SnippetWorker.perform_async(@snippet.id)
       redirect_to @snippet
     end
   end
